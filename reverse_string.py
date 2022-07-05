74bcad7705a0bb174156266fbd3025c4553142b2
superhughhh <odekerken.h@gmail.com>
2022-07-05 07:57:16 +0100

    add new algo

---
+++ reverse_string.py
@@ -0,0 +1,15 @@
+#this algo allow to reverse a str
+
+#for example : hello => olleh
+
+my_str = "bonjour"
+
+def reverse_string(str):
+    result = ""
+    for letter in str:
+        result = letter + result
+    return result
+    
+
+inv_str = reverse_string(my_str)
+print(inv_str)
