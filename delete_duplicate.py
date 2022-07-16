fe55b8716c1b37919bc076527c67e9c912ec0f7c
superhughhh <odekerken.h@gmail.com>
2022-07-16 19:01:38 +0100

    new storage system

--- delete_duplicate.py
+++
@@ -1,8 +0,0 @@
-#here, i just use a set collection, so easy !
-
-list_names = ["Pat", "Rick", "Frank", "Bryan", "Scott", "Scott", "Peter", "Bryan"]
-
-set_names = set(list_names)
-print(set_names)
-
-#be careful, because the order isn't respect when we use set
